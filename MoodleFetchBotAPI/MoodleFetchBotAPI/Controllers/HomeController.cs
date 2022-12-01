4de9076fd0f1093f2a58f6d709ba59d6a9fd37c0
bobost2 <bobisfe@gmail.com>
2022-12-01 19:22:32 +0200

    Initial commit

---
+++ MoodleFetchBotAPI/MoodleFetchBotAPI/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoodleFetchBotAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("TestRequest")]
+        public IActionResult TestRequest()
+        {
+            return Json(new {test = "yes"});
+        }
+    }
+}
